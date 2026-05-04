853e594b5f8c24b84ad005bd1c4c6718d685d8f0
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-05 01:54:32 +0200

    solve excercise 4 from array methods

--- 04-Sort in decreasing order.js
+++ 04-Sort in decreasing order.js
@@ -7,4 +7,9 @@ let arr = [5, 2, 1, -10, 8];
 
 alert( arr ); // 8, 5, 2, 1, -10
 `
-alert("exercise 4")
+let arr = [5, 2, 1, -10, 8];
+
+// ... your code to sort it in decreasing order
+arr.sort((a,b) => b-a)
+
+alert( arr ); // 8, 5, 2, 1, -10
