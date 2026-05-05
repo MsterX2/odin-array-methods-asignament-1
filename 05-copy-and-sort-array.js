38d2c3aec036a25d0e0346ff6ed42e03bde619ac
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-05 02:01:03 +0200

    solve excercise 5 from array methods

--- 05-copy-and-sort-array.js
+++ 05-copy-and-sort-array.js
@@ -4,6 +4,12 @@ importance: 5
 We have an array of strings arr. We’d like to have a sorted copy of it, but keep arr unmodified.
 
 Create a function copySorted(arr) that returns such a copy.
+`
+
+function copySorted(arr) {
+    newArray = arr.slice()
+    return newArray.sort()
+}
 
 let arr = ["HTML", "JavaScript", "CSS"];
 
@@ -11,5 +17,3 @@ let sorted = copySorted(arr);
 
 alert( sorted ); // CSS, HTML, JavaScript
 alert( arr ); // HTML, JavaScript, CSS (no changes)
-`
-alert("exercise 5")
