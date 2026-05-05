d073bc48a6bd061f796cb3290d5316b95c8d06b0
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-05 02:39:07 +0200

    solve excercise 6

--- 06-Shuffle an array.js
+++ 06-Shuffle an array.js
@@ -19,4 +19,23 @@ shuffle(arr);
 All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] 
 or [3,1,2] etc, with equal probability of each case.
 `
-alert("exercise 6")
+function get_first_n_no_negative_numbers(n) {
+    return Math.floor(Math.random() * n)
+}
+function shuffle(arr) {
+    let arrCopy = arr.slice();
+    let newArr = [];
+    while(arrCopy.length > 0) {
+        if (arrCopy.length === 1) {
+            newArr.push(arrCopy.pop())
+            continue
+        }
+        let randomIndex = get_first_n_no_negative_numbers(arr.length)
+        let randomItem = arrCopy.splice(randomIndex, 1)
+        newArr.push(randomItem[0])
+    }
+    return newArr
+}
+let arr = [1, 2, 3];
+
+alert(shuffle(arr));
