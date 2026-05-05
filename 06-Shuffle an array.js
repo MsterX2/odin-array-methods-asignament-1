8294e60583651ac3025b722e4d1712627077a39f
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2026-05-05 04:03:41 +0200

    solve excercise 6 and 7

--- 06-Shuffle an array.js
+++ 06-Shuffle an array.js
@@ -36,6 +36,40 @@ function shuffle(arr) {
     }
     return newArr
 }
-let arr = [1, 2, 3];
 
-alert(shuffle(arr));
+function shuffle2(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+let count = {
+  '123': 0,
+  '132': 0,
+  '213': 0,
+  '231': 0,
+  '321': 0,
+  '312': 0
+};
+
+let count2 = {
+  '123': 0,
+  '132': 0,
+  '213': 0,
+  '231': 0,
+  '321': 0,
+  '312': 0
+};
+
+for (let i = 0; i < 1000000; i++) {
+  let array = [1, 2, 3];
+  let newArr = shuffle(array);
+  count[newArr.join('')]++;
+  shuffle2(array);
+  count2[array.join('')]++;
+}
+
+// show counts of all possible permutations
+console.log({count})
+console.log({count2})
--- 07-Filter unique array members.js
+++ 07-Filter unique array members.js
@@ -5,14 +5,19 @@ Let arr be an array.
 Create a function unique(arr) that should return an array with unique items of arr.
 
 For instance:
+`
 
 function unique(arr) {
   /* your code */
+  let uniqueStrings = []
+  for (let string of arr) {
+    if (!uniqueStrings.includes(string)) uniqueStrings.push(string)
+  }
+  return uniqueStrings
 }
 
 let strings = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 
-alert( unique(strings) ); // Hare, Krishna, :-O`
-alert("exercise 7")
+alert( unique(strings) ); // Hare, Krishna, :-O
